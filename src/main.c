342a2fc4fb8e0339f611d4ee11571b790e90d28a
Lance Hart <shipsimfan@hotmail.com>
2021-07-12 22:01:50 -0400

    Added execution of second program

--- src/main.c
+++ src/main.c
@@ -3,5 +3,10 @@
 int main() {
     console_write_str("Hello world!\n");
 
+    ProcessID pid = execute("1:/LOS/CAT.APP");
+    wait_process(pid);
+
+    console_write_str("Cat exited!\n");
+
     return 0;
 }
