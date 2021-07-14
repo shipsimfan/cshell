72498c4825a303fd1bb131000d21aaeffd69810b
Lance Hart <shipsimfan@hotmail.com>
2021-07-14 00:15:35 -0400

    Added event loop

--- src/main.c
+++ src/main.c
@@ -3,16 +3,14 @@
 #include <stdlib.h>
 
 int main() {
-    uint64_t* ptr = (uint64_t*)malloc(sizeof(uint64_t));
+    printf("Hello world!\n");
 
-    *ptr = 0xABCDEF;
-
-    printf("Hello World!\n%#lX\n", *ptr);
-
-    ProcessID pid = execute("1:/LOS/CAT.APP");
-    uint64_t status = wait_process(pid);
-
-    printf("Cat exited with status %#lX\n", status);
-
-    return 0x12345678;
+    Event e;
+    while (1) {
+        while (peek_event(&e) != 0) {
+            if (e.type == 1) {
+                printf("%c", (char)e.param1);
+            }
+        }
+    }
 }
